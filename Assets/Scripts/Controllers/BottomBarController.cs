29cf8b7e2680b284ee48cdfbb772632dd2b28734
☢️Ailec Shikshaliar☣️ <151162917+AilecShikShaliar@users.noreply.github.com>
2024-09-18 17:58:35 +0200

    Initial commit

---
+++ Assets/Scripts/Controllers/BottomBarController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BottomBarController : MonoBehaviour
+{
+    public TextMeshProUGUI barText;
+    public TextMeshProUGUI personNameText;
+    private int _sentenceIndex = -1;
+    public StoryScene currentScene;
+    private State state =State.COMPLETED;
+
+    private enum State 
+    {
+        PLAYING, COMPLETED
+    }
+
+    public void PlayScene (StoryScene scene)
+    {
+        currentScene = scene;
+        _sentenceIndex = -1;
+        PlayNextSentence();
+    }
+    public void PlayNextSentence()
+    {
+        StartCoroutine(TypeText(currentScene.sentences[++_sentenceIndex].text));
+        personNameText.text = currentScene.sentences[_sentenceIndex].speaker.speakerName;
+        personNameText.color = currentScene.sentences[_sentenceIndex].speaker.textColor;
+    }
+
+    public bool IsCompleted()
+    {
+        return state == State.COMPLETED;
+    }
+
+    public bool IsLastSentence()
+    {
+        return _sentenceIndex + 1 == currentScene.sentences.Count;
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        barText.text = "";
+        state = State.PLAYING;
+        int wordIndex = 0;
+
+        while (state != State.COMPLETED)
+        {
+            barText.text += text[wordIndex];
+            yield return new WaitForSeconds(0.05f);
+            if(++wordIndex == text.Length)
+            {
+                state = State.COMPLETED;
+                break;
+            }
+
+        }
+    } 
+}
